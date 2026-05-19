02c6fb7deea82d70793f459650b5812299e88557
Chang feng <rodneychantelle46@gmail.com>
2026-05-19 22:32:30 +0800

    add day2 profile variables

---
+++ week-1/day2_profile.py
@@ -0,0 +1,13 @@
+person = {
+"name" : "陆恒成",
+"major" : "研究生",
+"age" : 23 ,
+"is_student" : True,
+"hobbies" : ["AI", "reading", "fitness" ]
+}
+print(person["name"])
+print(person["major"])
+print(person["age"])
+print(person["hobbies"][0])
+print("我是", person["name"], "专业是", person["major"], "今年", person["age"], "岁")
+print("我的第一个爱好是:", person["hobbies"][0])
