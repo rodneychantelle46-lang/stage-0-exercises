36f2106a96a62b258b27d13b74f9885101ed8e94
Chang feng <rodneychantelle46@gmail.com>
2026-06-08 20:14:25 +0800

    add day9 todo item methods

--- week-2/day8_oop.py
+++ week-2/day8_oop.py
@@ -2,10 +2,13 @@ class TodoItem:
     def __init__(self, title):
         self.title = title
         self.done = False
+    def mark_done(self):
+        self.done = True
+    def show(self):
+        print(self.title, self.done)
 
-task = TodoItem("学习文件读写")
-homework = TodoItem("学习异常处理")
-
-print(task.title,  task.done)
-print(homework.title, homework.done)
-
+task1 = TodoItem("学习文件读写")
+task2 = TodoItem("学习异常处理")
+task1.mark_done()
+task1.show()
+task2.show()
