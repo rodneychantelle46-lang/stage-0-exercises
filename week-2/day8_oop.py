321b7c8aa7dfc202b073d42a96d3c4628dda8574
Chang feng <rodneychantelle46@gmail.com>
2026-06-09 11:27:23 +0800

    add day10 todo list class

--- week-2/day8_oop.py
+++ week-2/day8_oop.py
@@ -7,8 +7,35 @@ class TodoItem:
     def show(self):
         print(self.title, self.done)
 
-task1 = TodoItem("学习文件读写")
-task2 = TodoItem("学习异常处理")
-task1.mark_done()
-task1.show()
-task2.show()
+class TodoList:
+    def __init__(self):
+        self.items = []
+
+    def add(self, item):
+        item = TodoItem(item)
+        self.items.append(item)
+
+    def show_all(self):
+        for item in self.items:
+            item.show()
+
+    def mark_done_by_index(self, index):
+        if 0 <= index < len(self.items):
+            self.items[index].mark_done()
+        else:
+            print("任务不存在")
+
+    def delete_by_index(self, index):
+        if 0 <= index < len(self.items):
+            del self.items[index]
+        else:
+            print("任务不存在")
+
+
+todo = TodoList()
+todo.add("学习AI")
+todo.add("学习Python")
+todo.show_all()
+todo.mark_done_by_index(1)
+todo.delete_by_index(0)
+todo.show_all()
